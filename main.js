03ac3d4ae121b0b48be13f3b938bed8db88c9d24
joabeplay1 <joabejonatas186@gmail.com>
2026-07-12 08:43:25 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 700,
+        minWidth: 900,
+        minHeight: 600,
+        title: "Internet Share Pro",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    win.loadFile("src/index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
